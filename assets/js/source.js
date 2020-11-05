16d640cea46c617b9ad8e8fb79aef18eef406cd8
johnsondev <johnson.dev298@gmail.com>
2020-11-05 08:05:10 +0700

    updated

--- assets/js/source.js
+++ assets/js/source.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
    $('.navbar-toggler').click(function(){
       $('#navbarSupportedContent').slideToggle(300);
    });
+    $(".upload-button").on('click', function() {
+        $(".file-upload").click();
+    });
 });
